c283a7609bb2595bf1b03cf8314d4336060f493e
thazinnaing <thazin@code2lab.com>
2024-04-04 10:31:01 +0630

    feat: edit and update method to comment table

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,6 +5,21 @@ class CommentsController < ApplicationController
         redirect_to article_path(@article)
     end
 
+    def edit
+        @article = Article.find(params[:article_id])
+        @comment = @article.comments.find(params[:id])
+    end
+
+    def update
+        @article = Article.find(params[:article_id])
+        @comment = @article.comments.find(params[:id])
+        if @comment.update(comment_params)
+            redirect_to article_path(@article)
+        else
+            render :edit, status: :unprocessable_entity
+        end
+    end
+
     def destroy
         @article = Article.find(params[:article_id])
         @comment = @article.comments.find(params[:id])
